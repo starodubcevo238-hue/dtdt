972afc2a29daaf93f07e4dd7506bdaa4e220aa99
starodubcevo238-hue <starodubcevo238@gmail.com>
2026-04-15 10:30:20 +0300

    Update cash-count.py

--- cash-count.py
+++ cash-count.py
@@ -1,65 +1,63 @@
+import json
+import os
 import sys
+DATA_FILE = "my_notes.json"
 
-def calc_sum(nums):
-    return sum(nums)
+def load_d():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+        return{}
+    
 
-def calc_mul(nums):
-    result = 1
-    for n in nums:
-        result *= n
-    return result
+def save_d(data):
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
-def calc_avg(nums):
-    return sum(nums) / len(nums)
+def add_entry(data):
+    name = input("введите имя:").strip()
+    Value = input("введите описание:").strip()
+    category = input("введите категорию:").strip().lower()
 
-args = sys.argv[1:]
+    if category not in data:
+        data[category] = {}
+    data[category][name] = Value
+    save_d(data)
+    print("добавлено!")
+    
+def show_all(data):
+    if not  data:
+        print("пусто")
+        return
+    for cat,items in data.items():
+        print(f"\n категория {cat}")
+        for name,desc in items.items():
+            print(f"\n  {name}:{desc}")    
 
-# Значения по умолчанию
-input_file = "input.txt"
-output_file = None
-operation = None
 
-# Разбор аргументов командной строки
-i = 0
-while i < len(args):
-    if args[i] == "--input":
-        if i + 1 < len(args) and not args[i+1].startswith('-'):
-            input_file = args[i+1]
-            i += 1
-    elif args[i] == "--output":
-        if i + 1 < len(args) and not args[i+1].startswith('-'):
-            output_file = args[i+1]
-            i += 1
+
+
+
+def main():
+    data = load_d()
+    while True:
+        print("\n--- МЕНЮ ---")
+        print("python expenses.py add. Добавить запись")
+        print("python expenses.py all list. Показать всё")
+        print("python expenses.py list. Показать по категории")
+        print("python expenses.py exit. Выход")
+        choice = input("Выберите действие: ").strip()
+
+        if choice == "python expenses.py add":
+            add_entry(data)
+        elif choice == "python expenses.py all list":
+            show_all(data)
+        elif choice == "python expenses.py list":
+            show_by_category(data)
+        elif choice == "python expenses.py exit":
+            print("До свидания!")            
+            break
         else:
-            output_file = "output.txt"   # имя по умолчанию, если следующего аргумента нет
-    elif args[i] in ("-sum", "-mul", "-arv"):
-        operation = args[i]
-    i += 1
-
-# Проверка наличия операции
-if operation is None:
-    print("Ошибка: не указана операция (-sum, -mul, -arv)")
-    sys.exit(1)
-
-# Чтение входного файла
-try:
-    with open(input_file, "r") as f:
-        line = f.readline()
-        numbers = list(map(int, line.split()))
-except FileNotFoundError:
-    print(f"Ошибка: файл '{input_file}' не найден.")
-    sys.exit(1)
-
-
-if operation == "-sum":
-    result = calc_sum(numbers)
-elif operation == "-mul":
-    result = calc_mul(numbers)
-else: 
-    result = calc_avg(numbers)
-
-if output_file:
-    with open(output_file, "w") as f:
-        f.write(str(result))
-else:
-    print(result)
+            print(" Неверный выбор.")
+if __name__ == "__main__":
+    main()
